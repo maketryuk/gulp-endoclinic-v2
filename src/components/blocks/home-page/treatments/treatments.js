f177849d5896a7042e936a0ff5c4d8be580e8dae
maketryuk <nevertry.design@gmail.com>
2022-02-01 13:59:09 +0200

    Add horizontal scroll with trigger in Treatments

--- src/components/blocks/home-page/treatments/treatments.js
+++ src/components/blocks/home-page/treatments/treatments.js
@@ -1,17 +1,52 @@
 // Media 992 =====>
 if (window.matchMedia("(min-width: 992px)").matches) {
-  // let treatmentsSlides = gsap.utils.toArray('.treatments-slide');
-  
-  // gsap.to(treatmentsSlides, {
-  //   xPercent: -100 * (treatmentsSlides.length - 1),
-  //   ease: 'none',
-  //   scrollTrigger: {
-  //     trigger: '.treatments',
-  //     pin: true,
-  //     scrub: 0.1,
-  //     end: () => '+=' + document.querySelector('.treatments').offsetWidth,
-  //   },
-  // });
+// Horizontal scroll in Treatments =====>
+gsap.registerPlugin(ScrollTrigger);
+
+const sections = gsap.utils.toArray(".treatments-wrapper");
+let maxWidth = 0;
+
+const getMaxWidth = () => {
+  maxWidth = 0;
+  sections.forEach((section) => {
+    maxWidth += section.offsetWidth;
+  });
+};
+getMaxWidth();
+ScrollTrigger.addEventListener("refreshInit", getMaxWidth);
+
+gsap.to(sections, {
+  x: () => `-${maxWidth - window.innerWidth}`,
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".treatments",
+    pin: true,
+    scrub: true,
+    end: () => `+=${maxWidth}`,
+    invalidateOnRefresh: true
+  }
+});
+
+sections.forEach((sct, i) => {
+  ScrollTrigger.create({
+    trigger: sct,
+    start: () => 'top top-=' + (sct.offsetLeft - window.innerWidth/2) * (maxWidth / (maxWidth - window.innerWidth)),
+    end: () => '+=' + sct.offsetWidth * (maxWidth / (maxWidth - window.innerWidth)),
+    toggleClass: {targets: sct, className: "active"}
+  });
+});
+
+// About labels =====>
+gsap.to(".about-label-table", {
+  yPercent: -200,
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".about-label-table",
+    // start: "top bottom", // the default values
+    // end: "bottom top",
+    scrub: true
+  }, 
+});
 
 
 
